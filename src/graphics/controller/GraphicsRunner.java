4a3a461c98c0b9611de0f7f0fc4d0bfe1df1f3a4
GAK00 <geran.kunz@gmail.com>
2016-11-01 07:53:24 -0600

    Built basic startup framework for windows project

---
+++ src/graphics/controller/GraphicsRunner.java
@@ -0,0 +1,11 @@
+package graphics.controller;
+
+public class GraphicsRunner {  
+	
+	public static void main(String[] args){
+		
+		GraphicsController controller = new GraphicsController();
+	}
+	
+
+}
