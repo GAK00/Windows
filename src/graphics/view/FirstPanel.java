1d537334d5b36220e0ea8aa4291a33f5e97cc12e
GAK00 <geran.kunz@gmail.com>
2016-11-01 09:49:52 -0600

    added a set hex value feature

--- src/graphics/view/FirstPanel.java
+++ src/graphics/view/FirstPanel.java
@@ -1,11 +1,14 @@
 package graphics.view;
 
 import java.awt.Color;
+
 import graphics.controller.GraphicsController;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import java.util.Random;
 import javax.swing.SpringLayout;
 
@@ -17,6 +20,7 @@ public class FirstPanel extends JPanel
 	private JButton randomButton;
 	private SpringLayout layout;
 	private Random rand;
+	private JTextField hexInput;
 	
 
 	public FirstPanel(GraphicsController controller)
@@ -26,7 +30,10 @@ public class FirstPanel extends JPanel
 		layout = new SpringLayout();
 		rand = new Random();
 		colorButton = new JButton("Color!");
-		
+		hexInput = new JTextField();
+		layout.putConstraint(SpringLayout.NORTH, hexInput, -1, SpringLayout.NORTH, colorButton);
+		layout.putConstraint(SpringLayout.WEST, hexInput, 25, SpringLayout.EAST, colorButton);
+		layout.putConstraint(SpringLayout.EAST, hexInput, 120, SpringLayout.EAST, colorButton);
 		randomButton = new JButton("Random");
 
 		
@@ -41,6 +48,7 @@ public class FirstPanel extends JPanel
 		this.setBackground(Color.CYAN);
 		this.add(colorButton);
 		this.add(randomButton);
+		this.add(hexInput);
 
 	}
 
@@ -77,7 +85,18 @@ public class FirstPanel extends JPanel
 		
 	}
 	private void colorButtonClicked(){
-		this.setBackground(Color.decode("#FFA167"));
+		if(hexInput.getText().equals("")){
+			this.setBackground(Color.WHITE);
+		}
+		else{
+			String hexCode;
+			if(!hexInput.getText().startsWith("#")){
+				hexCode = "#"+hexInput.getText();
+			}
+			else{
+				hexCode = hexInput.getText();
+			}
+		this.setBackground(Color.decode(hexCode));}
 	}
 
 }
